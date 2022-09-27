ae45494f30af4fbd1f73be06e2ba5e6741801f76
HaydenLeDev <loicroth.pro@gmail.com>
2022-09-27 12:30:05 +0200

    filtre appareil

--- script/utils/display.js
+++ script/utils/display.js
@@ -1,6 +1,7 @@
 import { recetteFactory } from "../factories/recettes.js";
 import { filtreFactory } from "../factories/filtre.js";
-export { displayRecettes, displayFiltreIngredients, displayFocus, noDisplayFocus };
+export { displayRecettes, displayFiltreIngredients, displayFocusIngredient, noDisplayFocusIngredient,
+    displayFocusAppareil, noDisplayFocusAppareil, displayFiltreAppareil};
 
 async function displayRecettes(data){
     const recettesSection = document.querySelector(".recettes");
@@ -26,15 +27,39 @@ async function displayFiltreIngredients(tab){
     });
 }
 
-async function displayFocus(){
+async function displayFiltreAppareil(tab){
+    const filtreIngredientsDom = document.querySelector("#filtre_secondaire_composants_appareil");
+    let nbMax = 0;
+    tab.forEach(el => {
+        if(nbMax < 30){
+            const filtreModel = filtreFactory(el);
+            const filtreElement = filtreModel.getFiltreButtonDOM();
+            filtreIngredientsDom.appendChild(filtreElement);
+            nbMax++;
+        }
+    });
+}
+
+async function displayFocusIngredient(){
     this.parentElement.parentElement.classList.add("filtre_secondaire_actif");
     document.getElementById("filtre_secondaire_composants_ingredient").style.display = "grid";
     document.getElementById("filtre_ingredients_input").placeholder = "Recherche un ingrédient";
-    document.get
 }
 
-async function noDisplayFocus(){
+async function noDisplayFocusIngredient(){
     this.parentElement.parentElement.classList.remove("filtre_secondaire_actif");
     document.getElementById("filtre_secondaire_composants_ingredient").style.display = "none";
-    document.getElementById("filtre_ingredients_input").placeholder = "Ingredient";
+    document.getElementById("filtre_ingredients_input").placeholder = "Ingrédients";
+}
+
+async function displayFocusAppareil(){
+    this.parentElement.parentElement.classList.add("filtre_secondaire_actif");
+    document.getElementById("filtre_secondaire_composants_appareil").style.display = "grid";
+    document.getElementById("filtre_appareils_input").placeholder = "Recherche d'un appareils";
+}
+
+async function noDisplayFocusAppareil(){
+    this.parentElement.parentElement.classList.remove("filtre_secondaire_actif");
+    document.getElementById("filtre_secondaire_composants_appareil").style.display = "none";
+    document.getElementById("filtre_appareils_input").placeholder = "Appareils";
 }
--- script/utils/filtre.js
+++ script/utils/filtre.js
@@ -1,84 +1,133 @@
-import { displayRecettes, displayFiltreIngredients, displayFocus, noDisplayFocus } from "../utils/display.js";
-export{initFiltre}
+import { displayRecettes, displayFiltreIngredients, noDisplayFocusIngredient, displayFocusIngredient,
+    noDisplayFocusAppareil, displayFocusAppareil, displayFiltreAppareil } from "../utils/display.js";
+export { initFiltre };
 
 
-
-function initFiltre(data){
+function initFiltre(data) {
     var tableauIngredients = [];
     tableauIngredients = tableauIngredientsInits(tableauIngredients, data);
-    document.getElementById("filtre_secondaire_composants_ingredient").innerHTML="";
+    document.getElementById("filtre_secondaire_composants_ingredient").innerHTML = "";
     displayFiltreIngredients(tableauIngredients);
     document.getElementById("filtre_titre_input").addEventListener('keyup', rechercheTitre);
-    document.getElementById("filtre_ingredients_input").addEventListener('keyup', rechercheFiltre);
-    document.getElementById("filtre_ingredients_input").addEventListener('focus', displayFocus);
-    document.getElementById("filtre_ingredients_input").addEventListener('blur', noDisplayFocus);
+    document.getElementById("filtre_ingredients_input").addEventListener('keyup', rechercheFiltreIngredient);
+    document.getElementById("filtre_ingredients_input").addEventListener('focus', displayFocusIngredient);
+    document.getElementById("filtre_ingredients_input").addEventListener('blur', noDisplayFocusIngredient);
 
-    function rechercheTitre(){
+    var tableauAppareil = [];
+    tableauAppareil = tableauAppareilsInit(tableauAppareil, data);
+    document.getElementById("filtre_secondaire_composants_appareil").innerHTML = "";
+    displayFiltreAppareil(tableauAppareil);
+    document.getElementById("filtre_appareils_input").addEventListener('keyup', rechercheFiltreAppareil);
+    document.getElementById("filtre_appareils_input").addEventListener('focus', displayFocusAppareil);
+    document.getElementById("filtre_appareils_input").addEventListener('blur', noDisplayFocusAppareil);
+
+
+    var tableauUstensiles = [];
+    tableauAppareil = tableauUstensilesInit(tableauAppareil, data);
+
+    function rechercheTitre() {
         let inputUser = this.value;
-        let result= [];
-        if (nombreCharMin(inputUser)){
-            document.getElementById("liste_recette").innerHTML="";
+        let result = [];
+        if (nombreCharMin(inputUser)) {
+            document.getElementById("liste_recette").innerHTML = "";
             data.forEach(element => {
                 console.log(element.name);
-                if (comparaisonString (element.name, inputUser)){
+                if (comparaisonString(element.name, inputUser)) {
                     result.push(element);
                 }
             });
             console.log(result);
-            if (result.length == 0 ){
+            if (result.length == 0) {
                 document.getElementById("recette_null").style.display = "flex";
-            } else{
+            } else {
                 document.getElementById("recette_null").style.display = "none";
                 displayRecettes(result);
             }
-            
+
         } else {
-            document.getElementById("liste_recette").innerHTML="";
+            document.getElementById("liste_recette").innerHTML = "";
             displayRecettes(data);
         }
     }
 
-    function rechercheFiltre(){
+    function rechercheFiltreIngredient() {
         let inputUser = this.value;
-        
         var result = [];
         tableauIngredients = tableauIngredientsInits(tableauIngredients, data);
-        document.getElementById("filtre_secondaire_composants_ingredient").innerHTML="";
+        document.getElementById("filtre_secondaire_composants_ingredient").innerHTML = "";
         displayFiltreIngredients(tableauIngredients);
-        if(nombreCharMin(inputUser)){
+        if (nombreCharMin(inputUser)) {
             tableauIngredients.forEach(element => {
-                if (comparaisonString (element, inputUser)){
+                if (comparaisonString(element, inputUser)) {
                     result.push(element);
                 }
             });
             console.log(result);
-            document.getElementById("filtre_secondaire_composants_ingredient").innerHTML="";
+            document.getElementById("filtre_secondaire_composants_ingredient").innerHTML = "";
             displayFiltreIngredients(result);
+        }
+    }
+
+    function rechercheFiltreAppareil() {
+        let inputUser = this.value;
+        var result = [];
+        tableauAppareil = tableauAppareilsInit(tableauAppareil, data);
+        document.getElementById("filtre_secondaire_composants_appareil").innerHTML = "";
+        displayFiltreAppareil(tableauAppareil);
+        if (nombreCharMin(inputUser)) {
+            tableauAppareil.forEach(element => {
+                if (comparaisonString(element, inputUser)) {
+                    result.push(element);
+                }
+            });
+            console.log(result);
+            document.getElementById("filtre_secondaire_composants_appareil").innerHTML = "";
+            displayFiltreAppareil(result);
         }
     }
 }
 
 
 
-function nombreCharMin(text){
-    if (text.length > 2){
+function nombreCharMin(text) {
+    if (text.length > 2) {
         return true;
     }
     return false;
 }
 
-function comparaisonString (text_1, text_2){
+function comparaisonString(text_1, text_2) {
     let text_1Up = text_1.toUpperCase();
     let text_2Up = text_2.toUpperCase();
     return text_1Up.includes(text_2Up);
 }
 
-function tableauIngredientsInits (tab, data){
+function tableauIngredientsInits(tab, data) {
     data.forEach(element => {
-        element.ingredients.forEach(i => {   
-            if(tab.indexOf(i.ingredient) === -1){
-                console.log(i.ingredient);
+        element.ingredients.forEach(i => {
+            if (tab.indexOf(i.ingredient) === -1) {
                 tab.push(i.ingredient);
+            }
+        });
+    });
+    return tab;
+}
+
+function tableauAppareilsInit(tab, data) {
+    data.forEach(element => {
+        if (tab.indexOf(element.appliance) === -1) {
+            tab.push(element.appliance);
+        }
+    });
+    return tab;
+}
+
+function tableauUstensilesInit(tab, data) {
+    data.forEach(element => {
+        element.ustensils.forEach(i => {
+            if (tab.indexOf(i.ustensils) === -1) {
+                console.log(i.ustensils);
+                tab.push(i.ustensils);
             }
         });
     });
